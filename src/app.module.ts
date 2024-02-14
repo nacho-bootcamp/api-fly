a3ec339ac00a8b0a189ce1ca081c13571a1ab23c
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2024-02-14 20:41:32 -0300

    conexion mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose'; // Agrega esta importación
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.developer'],
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.URI_MONGODB), // Corrige la configuración de MongooseModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
