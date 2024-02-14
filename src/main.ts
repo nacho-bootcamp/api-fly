afcf818e84fbc63f990025662ce6bd88a91e9573
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2024-02-14 20:32:36 -0300

    Interceptor Global

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ExcepcionGlobal } from './common/excepcion/excepcionGlobal';
+import { TimeOutInterceptor } from './common/intercepcion/timeOut.Interceptors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new ExcepcionGlobal())
+  app.useGlobalFilters(new ExcepcionGlobal());
+  app.useGlobalInterceptors(new TimeOutInterceptor())
   await app.listen(3000);
 }
 bootstrap();
